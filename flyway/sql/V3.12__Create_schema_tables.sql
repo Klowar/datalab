25ac06ba953800b4c7b1b574316b66c1f73db60f
Aider <ideeer4@gmail.com>
2020-03-31 19:21:55 +0300

    SQl for create foreign tables

---
+++ flyway/sql/V3.12__Create_schema_tables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS fdw0.book (
+    id integer,
+    name varchar
+);
+
+CREATE TABLE IF NOT EXISTS fdw1.book (
+    id integer,
+    name varchar
+);
+
+CREATE TABLE IF NOT EXISTS fdw2.book (
+    id integer,
+    name varchar
+);
+
---
+++ flyway/sql/V3.13__Setup_postgresql_fdw.sql
@@ -0,0 +1,13 @@
+CREATE EXTENSION IF NOT EXISTS postgres_fdw;
+
+CREATE SERVER foreign_server FOREIGN DATA WRAPPER postgres_fdw OPTIONS (
+    host 'ec2-35-175-225-239.compute-1.amazonaws.com',
+    port '5432',
+    dbname 'foreigndb'
+);
+
+CREATE USER MAPPING FOR local_user SERVER foreign_server OPTIONS (
+    USER 'foreignuser',
+    PASSWORD '1234post'
+);
+
