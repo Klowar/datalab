4bc6789a1e52336a544427cb57929d8037940615
Aider <ideeer4@gmail.com>
2020-04-01 15:25:11 +0300

    Clean

--- flyway/sql/V3.1__Create_product_main_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS benchmark.products (
-    id integer,
-    name varchar
-);
-
--- flyway/sql/V3.7__Insert_product_table.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO benchmark.products
-SELECT
-    generate_series(1, 1000000) AS id,
-    'amazon :(' AS name;
-
