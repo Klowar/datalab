986746eaedda3997232793e256014643d65b7e99
Aider <ideeer4@gmail.com>
2020-04-06 15:40:06 +0300

    Lab4 - vacuum

---
+++ flyway/user/sql/V4.0__Create_vacuum_heap_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS benchmark.plane (
+    id integer,
+    name varchar
+)
+WITH (
+    autovacuum_enabled = FALSE
+);
+
---
+++ flyway/user/sql/V4.1__Insert_row_to_vacuum_table.sql
@@ -0,0 +1,3 @@
+INSERT INTO benchmark.plane (id, name)
+    VALUES (1, "boeing p-8 poseidon");
+
