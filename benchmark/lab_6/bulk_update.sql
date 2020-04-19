03028177774637f4cca2529f8959056af8490a1a
Aider <ideeer4@gmail.com>
2020-04-20 00:40:06 +0300

    Pgbench script

---
+++ benchmark/lab_6/bulk_update.sql
@@ -0,0 +1,12 @@
+\set id_1 random(1,1000000)
+\set id_2 random(1,1000000)
+\set txt random(1,1000000)
+BEGIN;
+UPDATE
+    datalab.order_hub
+SET
+    id = :id_1,
+    name = md5(:txt::text)
+WHERE
+    id = :id_2;
+END;
