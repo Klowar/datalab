f98fec3dcc1dee512447a13b0e5773ac0bb56f93
Aider <ideeer4@gmail.com>
2020-04-20 23:48:03 +0300

    Fixes

--- benchmark/lab_6/bulk_update.sql
+++ benchmark/lab_6/bulk_update.sql
@@ -1,11 +1,11 @@
 \set id_1 random(1,1000000)
-\set day random(0,6)
+\set d random(0,6)
 \set txt random(1,1000000)
 BEGIN;
 UPDATE
     order_hub
 SET
-    delivery = '2020_4_20'::date + day,
+    delivery = '2020_4_20'::date + :d,
     name = md5(:txt::text)
 WHERE
     id = :id_1;
