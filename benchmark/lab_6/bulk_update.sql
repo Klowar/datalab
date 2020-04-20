2f6addb37be84bccfd12a1656f1a5a6acdf53d82
Aider <ideeer4@gmail.com>
2020-04-20 23:47:17 +0300

    update benchmark script

--- benchmark/lab_6/bulk_update.sql
+++ benchmark/lab_6/bulk_update.sql
@@ -1,12 +1,12 @@
 \set id_1 random(1,1000000)
-\set id_2 random(1,1000000)
+\set day random(0,6)
 \set txt random(1,1000000)
 BEGIN;
 UPDATE
     order_hub
 SET
-    id = :id_1,
+    delivery = '2020_4_20'::date + day,
     name = md5(:txt::text)
 WHERE
-    id = :id_2;
+    id = :id_1;
 END;
