c7ef0c6219a08b595c4a9b3407036470808716a8
Aider <ideeer4@gmail.com>
2020-05-04 17:38:47 +0300

    Flyway hates comment changing

--- flyway/postgresEC2/sql/V7.2__Create_all_indexes.sql
+++ flyway/postgresEC2/sql/V7.2__Create_all_indexes.sql
@@ -1,9 +1,9 @@
 -- Btree
 CREATE INDEX IF NOT EXISTS panda_btree ON pandas_1 USING btree (name);
 
--- Hash
+-- Btree
 CREATE INDEX IF NOT EXISTS panda_hash ON pandas_2 USING HASH (name);
 
--- Brin
+-- Btree
 CREATE INDEX IF NOT EXISTS panda_brin ON pandas_3 USING BRIN (name);
 
