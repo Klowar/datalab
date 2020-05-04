506a20bf7d95bf68fdeb84b20fc5ceeb9ab393e2
Aider <ideeer4@gmail.com>
2020-05-04 16:03:19 +0300

    Lab 5 prepared statement

---
+++ flyway/postgresEC2/sql/V6.1__Create_tables.sql
@@ -0,0 +1,12 @@
+-- 1-st table
+CREATE TABLE IF NOT EXISTS fruits (
+    id integer,
+    name varchar
+);
+
+-- 2-nd table
+CREATE TABLE IF NOT EXISTS meats (
+    id integer,
+    name varchar
+);
+
