495b8ddf91d3754f3bd7f9534b61ca5af680d1af
Aider <ideeer4@gmail.com>
2020-05-04 16:09:28 +0300

    Fix typo in missing ;

--- benchmark/lab_5/insert_prepared.sql
+++ benchmark/lab_5/insert_prepared.sql
@@ -1,209 +1,107 @@
 \set id random(1,1000000)
 \set name random(1,1000000)
-BEGIN
-    PREPARE meat_insert (numeric, text) AS
-    INSERT INTO meats (id, name)
-        VALUES ($1, $2);
-
+BEGIN;
+PREPARE meat_insert (numeric, text) AS
+INSERT INTO meats (id, name)
+    VALUES ($1, $2);
 EXECUTE meat_insert (:id + 2, md5(:name::text));
-
 EXECUTE meat_insert (:id + 3, md5(:name::text));
-
 EXECUTE meat_insert (:id + 4, md5(:name::text));
-
 EXECUTE meat_insert (:id + 5, md5(:name::text));
-
 EXECUTE meat_insert (:id + 6, md5(:name::text));
-
 EXECUTE meat_insert (:id + 7, md5(:name::text));
-
 EXECUTE meat_insert (:id + 8, md5(:name::text));
-
 EXECUTE meat_insert (:id + 9, md5(:name::text));
-
 EXECUTE meat_insert (:id + 10, md5(:name::text));
-
 EXECUTE meat_insert (:id + 11, md5(:name::text));
-
 EXECUTE meat_insert (:id + 12, md5(:name::text));
-
 EXECUTE meat_insert (:id + 13, md5(:name::text));
-
 EXECUTE meat_insert (:id + 14, md5(:name::text));
-
 EXECUTE meat_insert (:id + 15, md5(:name::text));
-
 EXECUTE meat_insert (:id + 16, md5(:name::text));
-
 EXECUTE meat_insert (:id + 17, md5(:name::text));
-
 EXECUTE meat_insert (:id + 18, md5(:name::text));
-
 EXECUTE meat_insert (:id + 19, md5(:name::text));
-
 EXECUTE meat_insert (:id + 20, md5(:name::text));
-
 EXECUTE meat_insert (:id + 21, md5(:name::text));
-
 EXECUTE meat_insert (:id + 22, md5(:name::text));
-
 EXECUTE meat_insert (:id + 23, md5(:name::text));
-
 EXECUTE meat_insert (:id + 24, md5(:name::text));
-
 EXECUTE meat_insert (:id + 25, md5(:name::text));
-
 EXECUTE meat_insert (:id + 26, md5(:name::text));
-
 EXECUTE meat_insert (:id + 27, md5(:name::text));
-
 EXECUTE meat_insert (:id + 28, md5(:name::text));
-
 EXECUTE meat_insert (:id + 29, md5(:name::text));
-
 EXECUTE meat_insert (:id + 30, md5(:name::text));
-
 EXECUTE meat_insert (:id + 31, md5(:name::text));
-
 EXECUTE meat_insert (:id + 32, md5(:name::text));
-
 EXECUTE meat_insert (:id + 33, md5(:name::text));
-
 EXECUTE meat_insert (:id + 34, md5(:name::text));
-
 EXECUTE meat_insert (:id + 35, md5(:name::text));
-
 EXECUTE meat_insert (:id + 36, md5(:name::text));
-
 EXECUTE meat_insert (:id + 37, md5(:name::text));
-
 EXECUTE meat_insert (:id + 38, md5(:name::text));
-
 EXECUTE meat_insert (:id + 39, md5(:name::text));
-
 EXECUTE meat_insert (:id + 40, md5(:name::text));
-
 EXECUTE meat_insert (:id + 41, md5(:name::text));
-
 EXECUTE meat_insert (:id + 42, md5(:name::text));
-
 EXECUTE meat_insert (:id + 43, md5(:name::text));
-
 EXECUTE meat_insert (:id + 44, md5(:name::text));
-
 EXECUTE meat_insert (:id + 45, md5(:name::text));
-
 EXECUTE meat_insert (:id + 46, md5(:name::text));
-
 EXECUTE meat_insert (:id + 47, md5(:name::text));
-
 EXECUTE meat_insert (:id + 48, md5(:name::text));
-
 EXECUTE meat_insert (:id + 49, md5(:name::text));
-
 EXECUTE meat_insert (:id + 50, md5(:name::text));
-
 EXECUTE meat_insert (:id + 51, md5(:name::text));
-
 EXECUTE meat_insert (:id + 52, md5(:name::text));
-
 EXECUTE meat_insert (:id + 53, md5(:name::text));
-
 EXECUTE meat_insert (:id + 54, md5(:name::text));
-
 EXECUTE meat_insert (:id + 55, md5(:name::text));
-
 EXECUTE meat_insert (:id + 56, md5(:name::text));
-
 EXECUTE meat_insert (:id + 57, md5(:name::text));
-
 EXECUTE meat_insert (:id + 58, md5(:name::text));
-
 EXECUTE meat_insert (:id + 59, md5(:name::text));
-
 EXECUTE meat_insert (:id + 60, md5(:name::text));
-
 EXECUTE meat_insert (:id + 61, md5(:name::text));
-
 EXECUTE meat_insert (:id + 62, md5(:name::text));
-
 EXECUTE meat_insert (:id + 63, md5(:name::text));
-
 EXECUTE meat_insert (:id + 64, md5(:name::text));
-
 EXECUTE meat_insert (:id + 65, md5(:name::text));
-
 EXECUTE meat_insert (:id + 66, md5(:name::text));
-
 EXECUTE meat_insert (:id + 67, md5(:name::text));
-
 EXECUTE meat_insert (:id + 68, md5(:name::text));
-
 EXECUTE meat_insert (:id + 69, md5(:name::text));
-
 EXECUTE meat_insert (:id + 70, md5(:name::text));
-
 EXECUTE meat_insert (:id + 71, md5(:name::text));
-
 EXECUTE meat_insert (:id + 72, md5(:name::text));
-
 EXECUTE meat_insert (:id + 73, md5(:name::text));
-
 EXECUTE meat_insert (:id + 74, md5(:name::text));
-
 EXECUTE meat_insert (:id + 75, md5(:name::text));
-
 EXECUTE meat_insert (:id + 76, md5(:name::text));
-
 EXECUTE meat_insert (:id + 77, md5(:name::text));
-
 EXECUTE meat_insert (:id + 78, md5(:name::text));
-
 EXECUTE meat_insert (:id + 79, md5(:name::text));
-
 EXECUTE meat_insert (:id + 80, md5(:name::text));
-
 EXECUTE meat_insert (:id + 81, md5(:name::text));
-
 EXECUTE meat_insert (:id + 82, md5(:name::text));
-
 EXECUTE meat_insert (:id + 83, md5(:name::text));
-
 EXECUTE meat_insert (:id + 84, md5(:name::text));
-
 EXECUTE meat_insert (:id + 85, md5(:name::text));
-
 EXECUTE meat_insert (:id + 86, md5(:name::text));
-
 EXECUTE meat_insert (:id + 87, md5(:name::text));
-
 EXECUTE meat_insert (:id + 88, md5(:name::text));
-
 EXECUTE meat_insert (:id + 89, md5(:name::text));
-
 EXECUTE meat_insert (:id + 90, md5(:name::text));
-
 EXECUTE meat_insert (:id + 91, md5(:name::text));
-
 EXECUTE meat_insert (:id + 92, md5(:name::text));
-
 EXECUTE meat_insert (:id + 93, md5(:name::text));
-
 EXECUTE meat_insert (:id + 94, md5(:name::text));
-
 EXECUTE meat_insert (:id + 95, md5(:name::text));
-
 EXECUTE meat_insert (:id + 96, md5(:name::text));
-
 EXECUTE meat_insert (:id + 97, md5(:name::text));
-
 EXECUTE meat_insert (:id + 98, md5(:name::text));
-
 EXECUTE meat_insert (:id + 99, md5(:name::text));
-
 EXECUTE meat_insert (:id + 100, md5(:name::text));
-
 DEALLOCATE meat_insert;
-
 END;
-
--- benchmark/lab_5/insert_raw.sql
+++ benchmark/lab_5/insert_raw.sql
@@ -1,305 +1,204 @@
 \set id random(1,1000000)
 \set name random(1,1000000)
-BEGIN
-    INSERT INTO fruits (id, name)
-        VALUES (:id + 1, md5(:name::text));
-
+BEGIN;
 INSERT INTO fruits (id, name)
-    VALUES (:id + 2, md5(:name::text));
-
+    VALUES (2 + 1, md5(100::text));
+INSERT INTO fruits (id, name)
+    VALUES (3 + 2, md5(8::text));
 INSERT INTO fruits (id, name)
     VALUES (:id + 3, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 4, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 5, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 6, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 7, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 8, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 9, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 10, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 11, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 12, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 13, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 14, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 15, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 16, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 17, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 18, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 19, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 20, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 21, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 22, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 23, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 24, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 25, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 26, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 27, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 28, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 29, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 30, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 31, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 32, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 33, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 34, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 35, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 36, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 37, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 38, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 39, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 40, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 41, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 42, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 43, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 44, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 45, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 46, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 47, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 48, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 49, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 50, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 51, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 52, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 53, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 54, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 55, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 56, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 57, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 58, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 59, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 60, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 61, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 62, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 63, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 64, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 65, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 66, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 67, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 68, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 69, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 70, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 71, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 72, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 73, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 74, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 75, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 76, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 77, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 78, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 79, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 80, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 81, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 82, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 83, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 84, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 85, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 86, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 87, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 88, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 89, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 90, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 91, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 92, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 93, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 94, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 95, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 96, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 97, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 98, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 99, md5(:name::text));
-
 INSERT INTO fruits (id, name)
     VALUES (:id + 100, md5(:name::text));
-
 END;
-
