f06255c598ab220886a85da190ba4d5a21c1d1c4
Aider <ideeer4@gmail.com>
2020-05-04 15:25:50 +0300

    More usable output and read/precent

--- benchmark/lab_5/select_stat.sql
+++ benchmark/lab_5/select_stat.sql
@@ -1,9 +1,10 @@
 SELECT
-    query,
+    LEFT (query,
+        30),
     calls,
     total_time,
     ROWS,
-    100.0 * shared_blks_hit / nullif (shared_blks_hit + shared_blks_read, 0) ashit_percent
+    100.0 * shared_blks_read / nullif (shared_blks_hit + shared_blks_read, 0) read_percent
 FROM
     pg_stat_statements
 ORDER BY
